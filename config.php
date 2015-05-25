0e9bea1ee23913ca71fa3e15eef0c6afab4f4f27
Cường <lmcuong25@gmail.com>
2015-05-25 15:52:57 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+define("HARAVAN_API_KEY", "17920187d99a47bff0f5fe937d4c4383");
+define("HARAVAN_SECRET", "d83c00c9f95923d9e3de91bc0c010238");
+define("REDIRECT_URI", "http://haravanapi.com/authenticate.php");
+define("HARAVAN_SCOPE", "read_products write_products read_orders read_customers");
+define("IS_EMBED_APP", true);
+
+//link cài app:  https://shopname.myharavan.com/admin/api/auth/?api_key=17920187d99a47bff0f5fe937d4c4383
+//link vào App Nhúng iframe  https://shopname.myharavan.com/admin/app#/embed/17920187d99a47bff0f5fe937d4c4383
+
+
+?>
